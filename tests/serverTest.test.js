3006f69d561471425c8999ea4de576c86e8b2686
Luis Henrique S <luishenriquesantana92@gmail.com>
2022-05-21 23:52:31 -0300

    Teste retorno URL/SERVER RGBtoHEX

--- tests/serverTest.test.js
+++ tests/serverTest.test.js
@@ -5,12 +5,27 @@ const request = require('request');
 describe("SERVER - RGB to Hex", () => {
 
     const url = "http://localhost:8080";
-  
+    const urlRGBtoHEX = "http://localhost:8080/rgbToHex?r=255&g=5&b=255";
+    const urlHEXtoRGB = "http://localhost:8080/hexToRgb?hex=11AB24";
+
     it("should return status 200", (ok) => {
       request(url, (err, res) => {
         expect(res.statusCode).to.equal(200);
         ok()
       })
     });
+    it("RGB para HEX retorno", (ok) => {
+        request(urlRGBtoHEX, (err, res) => {
+          expect(res.body).to.equal('#ff05ff');
+          ok()
+        })
+      });
+      it("HEX para RGB  retorno", (ok) => {
+        request(urlHEXtoRGB, (err, res) => {
+          expect(res.body).to.equal([17, 171, 36]);
+          ok()
+        })
+      });
+    
       
 });
